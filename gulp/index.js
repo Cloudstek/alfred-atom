d11e96522680e4f549b3ae1146647fc0e2e10055
Maarten de Boer <maarten@cloudstek.nl>
2017-04-20 06:51:51 +0200

    Update package.json and gulp tasks

--- gulp/index.js
+++ gulp/index.js
@@ -1,20 +1,21 @@
-var gulp = require('gulp'),
-    util = require('gulp-util');
+'use strict';
+const gulp = require('gulp');
+const util = require('gulp-util');
 
-module.exports = function(tasks) {
-    tasks.forEach(function(name) {
-        var task = require('./tasks/' + name);
+module.exports = tasks => {
+    tasks.forEach(name => {
+        const task = require('./tasks/' + name);
 
         // Task function only
-        if(typeof task == "function") {
+        if (typeof task === 'function') {
             gulp.task(name, task);
             return;
         }
 
         // Task dependencies or task function and dependencies
-        if(task instanceof Array && task.length > 0) {
+        if (task instanceof Array && task.length > 0) {
             // Task function with dependencies
-            if(task.length >= 2 && task[0] instanceof Array && typeof task[task.length - 1] == "function") {
+            if (task.length >= 2 && task[0] instanceof Array && typeof task[task.length - 1] === 'function') {
                 gulp.task(name, task[0], task[task.length - 1]);
                 return;
             }
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,6 +1,7 @@
-var gulp = require('gulp');
+'use strict';
+const gulp = require('gulp');
 
-var task = function (done) {
+let task = done => {
     gulp.watch(['./**/*.js.flow', '!node_modules', '!node_modules/**'], ['babel']);
 };
 
